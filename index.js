540920f2b4131b8090c0e1a97357a5333bc8b3c3
Chriszilla <Chriszilla@users.noreply.github.com>
2023-07-05 11:36:02 +0200

    f

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import WebXRPolyfill from "https://cdn.jsdelivr.net/npm/webxr-polyfill@latest/build/webxr-polyfill.module.js";
 import * as THREE from 'three';
 const polyfill = new WebXRPolyfill();
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 let canvas = null;
 let gl = null;

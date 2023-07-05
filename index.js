3548cf7668aa9956b57cd401c210244917ced10c
Chriszilla <Chriszilla@users.noreply.github.com>
2023-07-05 11:07:19 +0200

    vr

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import WebXRPolyfill from "https://cdn.jsdelivr.net/npm/webxr-polyfill@latest/build/webxr-polyfill.module.js";
+const polyfill = new WebXRPolyfill();
+
 let canvas = null;
 let gl = null;
 
@@ -18,6 +21,50 @@ function initWebXR() {
     }
 }
 
+function onButtonClicked(){
+    if(!xrSession){
+        navigator.xr.requestSession("immersive-vr").then(onSessionStarted);
+    }
+    else{
+        xrSession.end();
+    }
+}
+
+function onSessionStarted(_session){
+    xrSession = _session;
+    xrSession.addEventListener("end", onSessionStarted);
+
+    initWebGL2({xrCompatible: true});
+
+    xrSession.updateRenderState({baseLayer: new XRWebGLLayer(xrSession, gl)});
+    xrSession.requestReferenceSpace("local").then((refSpace) => {
+        xrRefSpace = refSpace;
+        xrSession.requestAnimationFrame(onSessionFrame);
+    });
+
+    function onSessionFrame(t, frame){
+        const session = frame.session;
+        session.requestAnimationFrame(onSessionFrame);
+        let pose = frame.getViewerPose(xrRefSpace);
+
+        if(pose){
+            let glLayer = session.renderState.baseLayer;
+
+            gl.bindFramebuffer(gl.FRAMEBUFFER, glLayer.framebuffer);
+            gl.clearColor(0.4, 0.7, 0.9, 1.0);
+            gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+            for(let view of pose.views){
+                let viewport = glLayer.getViewport(view);
+                gl.viewport(viewport.x, viewport.y, viewport.width, viewport.height);
+            }
+        }
+    }
+
+    function onSessionEnded(){
+        xrSession = null;
+    }
+}
+
 function onResize(){
     canvas.width = canvas.clientWidth * window.devicePixelRatio;
     canvas.height = canvas.clientHeight * window.devicePixelRatio;
@@ -26,12 +73,12 @@ window.onResize = onResize;
 
 function initWebGL2(){
     canvas = document.createElement("canvas");
-    gl = canvas.getContext("webgl2");
+    gl = canvas.getContext("webgl2", attributes || {alpha: false});
     if(!gl){
         alert("This browser does not support WebGL2.");
         return;
     }
-    canvas.style="position:absolute; width: 100%; height: 100%; z-index:-1;"
+    canvas.style="position:absolute; width: 100%; height: 100%; left:0; top:0; right:0; bottom:0; margin: 0; z-index:-1;"
     document.body.appendChild(canvas);
     onResize();
 
@@ -39,4 +86,4 @@ function initWebGL2(){
     gl.clear(gl.COLOR_BUFFER_BIT);
 }
 
-initWebGL2();
+initWebXR();

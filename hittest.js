cf5c953a7e85955faa9bb7a89c0d41591c0008c1
Chriszilla <Chriszilla@users.noreply.github.com>
2023-07-06 15:53:44 +0200

    sad

--- hittest.js
+++ hittest.js
@@ -1,8 +1,10 @@
+let xrButton = document.getElementById("xr-button");
+
 async function activateXR() {
     // Add a canvas element and initialize a WebGL context that is compatible with WebXR.
     const canvas = document.createElement("canvas");
     document.body.appendChild(canvas);
     const gl = canvas.getContext("webgl", {xrCompatible: true});
-  
+    xrButton.textContent = "Hat nicht geklappt";
     // To be continued in upcoming steps.
   }

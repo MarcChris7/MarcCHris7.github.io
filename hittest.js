8f4d755b5456a315751ea4c37c789cee0378ac87
Chriszilla <Chriszilla@users.noreply.github.com>
2023-07-06 16:22:41 +0200

    dsa

--- hittest.js
+++ hittest.js
@@ -1,6 +1,7 @@
-let xrButton = document.getElementById("Header");
+
 
 function activateXR() {
+    let xrButton = document.getElementById("Header");
     xrButton.textContent = "Hat nicht geklappt";
     // Add a canvas element and initialize a WebGL context that is compatible with WebXR.
   

1e2c7b3ca34e9e77ef12d47fda79eb0849a956de
Chriszilla <Chriszilla@users.noreply.github.com>
2023-07-06 16:57:25 +0200

    asd

--- hittest.js
+++ hittest.js
@@ -1,8 +1,11 @@
 var header = document.getElementById("header");
-function activateXR() {
+async function activateXR() {
     
     //let xrButton = document.getElementById("Header");
     header.textContent  = "Hat nicht geklappt";
+    const canvas = document.createElement("canvas");
+    document.body.appendChild(canvas);
+    const gl = canvas.getContext("webgl", {xrCompatible: true});
     // Add a canvas element and initialize a WebGL context that is compatible with WebXR.
   
     // To be continued in upcoming steps.

cae0695f1bb940fbc38b8a49fc71d7a7d2d75fc2
Chriszilla <Chriszilla@users.noreply.github.com>
2023-07-06 15:51:05 +0200

    sda

--- hittest.js
+++ hittest.js
@@ -1,8 +1,8 @@
-import { VRButton } from 'three/addons/webxr/VRButton.js';
-document.body.appendChild( VRButton.createButton( renderer ) );
-renderer.xr.enabled = true;
-renderer.setAnimationLoop( function () {
-
-	renderer.render( scene, camera );
-
-} );
+async function activateXR() {
+    // Add a canvas element and initialize a WebGL context that is compatible with WebXR.
+    const canvas = document.createElement("canvas");
+    document.body.appendChild(canvas);
+    const gl = canvas.getContext("webgl", {xrCompatible: true});
+  
+    // To be continued in upcoming steps.
+  }
